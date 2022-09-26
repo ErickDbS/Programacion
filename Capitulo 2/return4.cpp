6841cef4fe608eaa372627cc6a45bec6e36b405a
ErickDbS <erickdelmedio@hotmail.com>
2022-09-25 20:55:13 -0600

    mas

---
+++ Capitulo 2/return4.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void printA()
+{
+	cout<<"A\n";
+}
+
+void printB()
+{
+	cout<<"B\n";
+}
+
+int main()
+{
+	printA();
+	printB();
+	
+	return 0;
+}
---
+++ Capitulo 2/return5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+void printA()
+{
+	cout<<"A\n";
+}
+
+int main()
+{
+	cout<<printA()<<"\n";
+	
+	return 0;
+}
